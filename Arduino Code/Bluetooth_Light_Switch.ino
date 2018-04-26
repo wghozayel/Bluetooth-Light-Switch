6976f526792f23a176b77b10c7010282222ed6cb
wghozayel <wghozayel@gmail.com>
2018-04-26 15:29:00 -0400

    Update Bluetooth_Light_Switch.ino

--- Arduino Code/Bluetooth_Light_Switch.ino
+++ Arduino Code/Bluetooth_Light_Switch.ino
@@ -2,27 +2,33 @@
 #include <Servo.h>
 
 Servo lightServo;
-Servo tvServo;
+int curtainsEn = 10;
+int curtainsIn1 = 6;
+int curtainsIn2 = 7;
 bool lightIsOn;  
+int val;
 
 void setup()
 {
   Serial.begin(9600);
   lightIsOn = true;
+  pinMode(curtainsEn, OUTPUT);
+  pinMode(curtainsIn1, OUTPUT);
+  pinMode(curtainsIn2, OUTPUT);
 }
 
 void loop() 
 { 
   while (!Serial.available());
   
-  int info = Serial.read();
+  int info = Serial.read(); 
   
   if(info == 255 && lightIsOn==true)
   {
     lightServo.attach(9);
     delay(15);
-    lightServo.write(25);  
-    delay(100);
+    lightServo.write(100);  
+    delay(430);
     lightServo.detach();
     lightIsOn = !lightIsOn;
   }
@@ -31,18 +37,27 @@ void loop()
     lightServo.attach(9);
     delay(15);
     lightServo.write(0);  
-    delay(100);
+    delay(430);
     lightServo.detach();
     lightIsOn = !lightIsOn;
   }
   else
   {
-    tvServo.attach(10);
-    delay(15);
-    tvServo.write(-15);  
-    delay(200);
-    tvServo.detach();
+    val = map(info, 0, 254, 0, 3);
+    Serial.println(val);
+    if(val == 0){
+      digitalWrite(curtainsIn1, HIGH);
+      digitalWrite(curtainsIn2, LOW);
+    }
+    else if(val ==1){
+      digitalWrite(curtainsIn1, LOW);
+      digitalWrite(curtainsIn2, LOW);
+    }
+    else if(val ==2){
+      digitalWrite(curtainsIn1, LOW);
+      digitalWrite(curtainsIn2, HIGH);
+    }
+    analogWrite(curtainsEn, 255);
   }
 
 } 
-
